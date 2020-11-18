01e733167e518b0aadcfd6c966e3f9b4bc1e2339
Danesh Badlani <38691121+daneshbadlani@users.noreply.github.com>
2020-11-19 03:04:04 +0500

    Add files via upload

---
+++ sbs.py
@@ -0,0 +1,189 @@
+
+import pandas as pd
+from io import StringIO
+from sklearn.impute import SimpleImputer
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn.base import clone
+from itertools import combinations
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.utils import resample
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+
+
+df = pd.read_csv("NewBioDeg.csv", header=None)
+
+df.columns = [ 
+    "SpMax_L",
+    "J_Dz(e)",
+    "nHM",
+    "F01[N-N]",
+    "F04[C-N]",
+    "NssssC",
+    "nCb-",
+    "C%",
+    "nCp",
+    "n0",
+    "F03[C-N]",
+    "SdssC",
+    "HyWi_B(m)",
+    "LOC",
+    "SM6_L",
+    "F03[C-0]",
+    "Me",
+    "Mi",
+    "nN-N",
+    "nArN02",
+    "nCRX3",
+    "SpPosA_B(p)",
+    "nCIR",
+    "B01[C-Br]",
+    "B03[C-Cl]",
+    "N-073",
+    "SpMax_A",
+    "Psi_i_1d",
+    "B04[C-Br]",
+    "Sd0",
+    "TI2_L",
+    "NCrt",
+    "C-026",
+    "F02[C-N]",
+    "nHDon",
+    "SpMax_B(m)",
+    "Psi_i_A",
+    "nN",
+    "SM6_B(m)",
+    "nArC00R",
+    "nX",
+    "ready"
+
+] 
+
+#perform imputation to fill in missing data
+df['ready'] = df['ready'].replace(to_replace={"NRB":0, "RB":1})
+df = df.fillna(df.mean())
+#imr_frequent = SimpleImputer(missing_values=np.nan)
+#imr_frequent = imr_frequent.fit(df[[i for i in list(df.columns) if i != "ready"]].values)
+#df[[i for i in list(df.columns) if i != "ready"]] = pd.DataFrame(data=imr_frequent.transform(df[[i for i in list(df.columns) if i != "ready"]].values))
+
+# code from https://elitedatascience.com/imbalanced-classes
+# Separate majority and minority classes
+df_majority = df[df.ready==0]
+df_minority = df[df.ready==1]
+ 
+# Upsample minority class
+df_minority_upsampled = resample(df_minority, 
+                                 replace=True,     # sample with replacement
+                                 n_samples=699,    # to match majority class
+                                 random_state=123) # reproducible results
+ 
+# Combine majority class with upsampled minority class
+df_upsampled = pd.concat([df_majority, df_minority_upsampled])
+
+X = df_upsampled[[i for i in list(df.columns) if i != "ready"]]
+y = df_upsampled['ready']
+
+
+
+X_train, X_test, y_train, y_test =    train_test_split(X, y, 
+                     test_size=0.3, 
+                     random_state=0, 
+                     stratify=y)
+
+# # Bringing features onto the same scale
+
+mms = MinMaxScaler()
+X_train_norm = mms.fit_transform(X_train)
+X_test_norm = mms.transform(X_test)
+
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+class SBS():
+    def __init__(self, estimator, k_features, scoring=accuracy_score,
+                 test_size=0.25, random_state=1):
+        self.scoring = scoring
+        self.estimator = clone(estimator)
+        self.k_features = k_features
+        self.test_size = test_size
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        
+        X_train, X_test, y_train, y_test =             train_test_split(X, y, test_size=self.test_size,
+                             random_state=self.random_state)
+
+        dim = X_train.shape[1]
+        self.indices_ = tuple(range(dim))
+        self.subsets_ = [self.indices_]
+        score = self._calc_score(X_train, y_train, 
+                                 X_test, y_test, self.indices_)
+        self.scores_ = [score]
+
+        while dim > self.k_features:
+            scores = []
+            subsets = []
+
+            for p in combinations(self.indices_, r=dim - 1):
+                score = self._calc_score(X_train, y_train, 
+                                         X_test, y_test, p)
+                scores.append(score)
+                subsets.append(p)
+
+            best = np.argmax(scores)
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            dim -= 1
+
+            self.scores_.append(scores[best])
+        self.k_score_ = self.scores_[-1]
+
+        return self
+
+    def transform(self, X):
+        return X[:, self.indices_]
+
+    def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+        self.estimator.fit(X_train[:, indices], y_train)
+        y_pred = self.estimator.predict(X_test[:, indices])
+        score = self.scoring(y_test, y_pred)
+        return score
+
+svm = SVC()
+
+# selecting features
+sbs = SBS(svm, k_features=1)
+sbs.fit(X_train_std, y_train)
+
+# plotting performance of feature subsets
+k_feat = [len(k) for k in sbs.subsets_]
+
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.02])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.tight_layout()
+plt.savefig("sbs.png")
+plt.show()
+
+k3 = list(sbs.subsets_[10])
+print(df.columns[1:][k3])
+
+svm.fit(X_train_std, y_train)
+print('Training accuracy:', svm.score(X_train_std, y_train))
+print('Test accuracy:', svm.score(X_test_std, y_test))
+
+svm.fit(X_train_std[:, k3], y_train)
+print('Training accuracy:', svm.score(X_train_std[:, k3], y_train))
+print('Test accuracy:', svm.score(X_test_std[:, k3], y_test))
